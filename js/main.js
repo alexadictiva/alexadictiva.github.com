b671383a9892545e5e8e16b3c80b81c3cc18675f
Libia Perez <alexandra220790@gmail.com>
2020-07-29 10:19:12 -0300

    verify mistakes and form paused

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+/*
 var nombre = document.getElementById("validationDefault01");
 var send = document.getElementById("send");
 send.addEventListener("click", enviarConsulta);
@@ -6,3 +7,4 @@ function enviarConsulta() {
   //se activa el método alert luego de 2 segundos
   setTimeout("alert('Su consulta fué enviada. Pronto será contactad@')", 2000);
 }
+*/
