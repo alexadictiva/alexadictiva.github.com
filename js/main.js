1910fb54f29aec8bb08686b7ed01a651d7c0346c
Libia Perez <alexandra220790@gmail.com>
2021-05-30 21:36:21 -0300

    added: About Me section with styles and Js code

--- js/main.js
+++ js/main.js
@@ -1,9 +1,35 @@
 var main = document.querySelector("main");
 var buttonModal = document.getElementById("buttonModal");
-var itemDescription = document.getElementById("itemDescription");
+
 buttonModal.addEventListener("clcik", buttonModal);
 
 function showModal() {
-  itemDescription.classList.toggle("modalPrueba");
-  /*  main.classList.toggle("overlayModal");*/
+  var itemDescription = document.getElementById("itemDescription");
+  itemDescription.classList.add("modalStyles");
+}
+
+function showModal1() {
+  var itemDescription1 = document.getElementById("itemDescription1");
+  itemDescription1.classList.add("modalStyles");
+}
+function showModal2() {
+  var itemDescription2 = document.getElementById("itemDescription2");
+  itemDescription2.classList.add("modalStyles");
+}
+function showModal3() {
+  var itemDescription3 = document.getElementById("itemDescription3");
+  itemDescription3.classList.add("modalStyles");
+}
+
+function closeButton() {
+  itemDescription.classList.remove("modalStyles");
+}
+function closeButton1() {
+  itemDescription1.classList.remove("modalStyles");
+}
+function closeButton2() {
+  itemDescription2.classList.remove("modalStyles");
+}
+function closeButton3() {
+  itemDescription3.classList.remove("modalStyles");
 }
