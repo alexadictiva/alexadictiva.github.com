1847318b59600c3915a97d21d22a2448002cf0de
Libia Perez <alexandra220790@gmail.com>
2021-06-01 22:33:29 -0300

    trying to add the modals code in js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var main = document.querySelector("main");
+/*var main = document.querySelector("main");
 var buttonModal = document.getElementById("buttonModal");
 
 buttonModal.addEventListener("clcik", buttonModal);
@@ -19,7 +19,7 @@ function showModal2() {
 function showModal3() {
   var itemDescription3 = document.getElementById("itemDescription3");
   itemDescription3.classList.toggle("modalStyles");
-}
+}*/
 
 /* function closeButton() {
   itemDescription.classList.remove("modalStyles");
@@ -33,3 +33,29 @@ function closeButton2() {
 function closeButton3() {
   itemDescription3.classList.remove("modalStyles");
 } */
+
+let overlay = document.createElement("div");
+let overlay2 = document.createElement("div");
+function showModal1() {
+  overlay.className = "overlay";
+  overlay.id = "overlay";
+  let itemDescription = document.querySelector(".itemDescription");
+  itemDescription.style.display = "block";
+  document.body.appendChild(overlay);
+}
+function showModal2() {
+  overlay2.className = "overlay";
+  overlay2.id = "overlay";
+  let itemDescription2 = document.querySelector(".itemDescription2");
+  itemDescription2.style.display = "block";
+  document.body.appendChild(overlay2);
+}
+
+function removeModal() {
+  itemDescription.style.display = "none";
+  document.body.removeChild(overlay);
+}
+function removeModal2() {
+  itemDescription2.style.display = "none";
+  document.body.removeChild(overlay2);
+}
