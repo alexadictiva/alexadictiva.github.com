604ba915f9b78d895d046422020a8f9686c1cdbb
Libia Perez <alexandra220790@gmail.com>
2021-06-02 22:59:15 -0300

    change the banner picture, and added two more button in about me section

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let overlay = document.createElement("div");
 let overlay2 = document.createElement("div");
+let overlay3 = document.createElement("div");
+let overlay4 = document.createElement("div");
+
 function showModal1() {
   overlay.className = "overlay";
   overlay.id = "overlay";
@@ -14,6 +17,20 @@ function showModal2() {
   itemDescription2.style.display = "block";
   document.body.appendChild(overlay2);
 }
+function showModal3() {
+  overlay3.className = "overlay";
+  overlay3.id = "overlay";
+  let itemDescription3 = document.querySelector(".itemDescription3");
+  itemDescription3.style.display = "block";
+  document.body.appendChild(overlay3);
+}
+function showModal4() {
+  overlay4.className = "overlay";
+  overlay4.id = "overlay";
+  let itemDescription4 = document.querySelector(".itemDescription4");
+  itemDescription4.style.display = "block";
+  document.body.appendChild(overlay4);
+}
 
 function removeModal() {
   itemDescription.style.display = "none";
@@ -22,4 +39,12 @@ function removeModal() {
 function removeModal2() {
   itemDescription2.style.display = "none";
   document.body.removeChild(overlay2);
+}
+function removeModal3() {
+  itemDescription3.style.display = "none";
+  document.body.removeChild(overlay3);
+}
+function removeModal4() {
+  itemDescription4.style.display = "none";
+  document.body.removeChild(overlay4);
 }
